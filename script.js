c654bcde26edff359db5ebe6050ca2c4b4dcf331
ssimmons122 <121777930+ssimmons122@users.noreply.github.com>
2023-02-12 23:58:50 -0500

    fixed arrays, still no inital button

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-// Assignment code here
+//JS password generator
 
-let generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 
 // Write password to the #password input
   // Arrays    
-  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-  var lowerCase = "abcdefghijklmnopqurstuvwxyz"
+  var upperCase = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  var lowerCase = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,u,r,s,t,u,v,w,x,y,z"
   var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~"
   var numeric = "0123456789"
 
@@ -17,18 +17,19 @@ let generateBtn = document.querySelector("#generate");
     
     // Prompts
   function generatePassword() {
-    
-    let pwLength = parseInt(
-      prompt("How long should your password be (Enter 8-128)?")
-    )
+    // length
+    //var pwLength = parseInt(
+      //prompt("How long should your password be (Enter 8-128)?")
+    //)
+    var length = Number(prompt("Enter the length of your password between 8-128 characters."))
   }
      
-    if (pwLength < 8 && pwLength > 128 ) {
+    if (pwLength > 8 && pwLength < 128 ) {
     } else { 
         alert ("Must have a minimum of 8 and max of 128 characters.");
       }
-    let upperCase = confirm ("Use upper case letters?");
-    let lowerCase = confirm ("Use lower case letters?");
+    let upperCaseL = confirm ("Use upper case letters?");
+    let lowerCaseL = confirm ("Use lower case letters?");
     let specialChar = confirm ("Use special characters?");
     let numeric = confirm ("Use numbers?");
     
@@ -41,6 +42,7 @@ let generateBtn = document.querySelector("#generate");
       alert ("Must select one or more character types to continue.")
     }
   
+
 
   passwordText.value = password;
 
